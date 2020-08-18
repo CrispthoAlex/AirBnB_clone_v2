47df0ec096f08b0f94d71d521cd539b4f4db4387
crispthoalex <alex877@gmail.com>
2020-08-18 14:21:12 +0000

    Fix exec block to location configuration

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,49 +1,13 @@
 # Bash script that sets up your web servers for the deployment of
 # web_static using Puppet
 
-exec { 'update':
-command  => 'sudo apt-get update',
-provider => shell,
-}
-
-package { 'nginx':
-ensure => installed,
-}
-
-file {['/data/', '/data/web_static', '/data/web_static2/releases',
-      '/data/web_static/releases/test', '/data/web_static/shared']:
-ensure => 'directory',
-owner  => 'ubuntu',
-group  => 'ubuntu',
-}
-
-file { '/data/web_static/releases/test/index.html':
-content => "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>",
-owner   => 'ubuntu',
-group   => 'ubuntu',
-}
-
-file { '/data/web_static/current':
-ensure => 'link',
-target => '/data/web_static/releases/test/',
-force  => yes,
-owner  => 'ubuntu',
-group  => 'ubuntu',
-}
-
-exec { 'sed':
-command => 'sudo sed -i "/listen 80 default_server/a location /hbnb_static/ {\
-\talias /data/web_static/current/;}" /etc/nginx/sites-available/default',
-require => Package['nginx'],
-}
-
-service { 'nginx':
-ensure  => running,
-require => Package['nginx'],
-}
+exec { '/usr/bin/env apt-get -y update':}
+exec { '/usr/bin/env apt-get -y install nginx':}
+exec { '/usr/bin/env mkdir -p /data/web_static/releases/test/':}
+exec { '/usr/bin/env mkdir -p /data/web_static/shared/':}
+exec { '/usr/bin/env echo "Hello Holberton School. No more Tiwetter" > /data/web_static/releases/test/index.html':}
+exec { '/usr/bin/env ln -sfv /data/web_static/releases/test/ /data/web_static/current':}
+exec { '/usr/bin/env sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n}" \
+     /etc/nginx/sites-available/default':}
+exec { '/usr/bin/env chown -hR ubuntu:ubuntu /data/':}
+exec { '/usr/bin/env service nginx restart':}
