e29150b63ccad6d0f40ffb56744245b9bf9e8aa3
crispthoalex <alex877@gmail.com>
2020-09-02 00:05:36 +0000

    Add public method close(self) for deserializing the JSON file to objects

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -62,3 +62,9 @@ class FileStorage:
             key = "{}.{}".format(type(obj).__name__, obj.id)
             if key in self.__objects:
                 del self.all()[key]
+
+    def close(self):
+        """ call reload() method for deserializing the JSON
+        file to objects
+        """
+        self.reload()
